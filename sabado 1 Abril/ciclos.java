982a415ecb2de2feeb331fdaf181ed82f5029442
Eduardo Queupumil F <e.queupumil01@ufromail.cl>
2017-04-01 12:21:59 -0300

    Update ciclos.java

--- sabado 1 Abril/ciclos.java
+++ sabado 1 Abril/ciclos.java
@@ -1,4 +1,4 @@
-    package ejemploclasesabado;
+package ejemploclaseSabadoSabado1Abril;
 
 import javax.swing.JOptionPane;
 import javax.swing.Icon;
@@ -8,26 +8,27 @@ public class ciclos {
 
     public static void main(String[] args) {
         System.out.println("+++++++++++++for++++++++++++++++");
-        for (int i = 0; i <= 10; i++) {
-            System.out.println("hola mundo: "+i);
-        }
+        for (int i = 0; i <= 10; i++) {//tipo de ciclo(for)(iniciar variable(int i=0); hasta cuanto se repite(i<=10); incremento(i++)) inicia ciclo({)
+            System.out.println("hola mundo: "+i);//muestra por pantalla
+        }//cerrar ciclo
         
         
         System.out.println("+++++++++++++do-while++++++++++++++++");
-        int cont=0;
+        int cont=0;//inicica variable(int cont=0;)
         
-        do {
-            System.out.println("hola mundo:"+cont);
+        do {//ciclo(do) inicia ciclo({)
+            System.out.println("hola mundo:"+cont);//muestra por pantalla
             cont++;
-        } while (cont < 10);
+            //aumento contador(cont++)
+        } while (cont < 10);//cierro ciclo(}) condicion(mientras cont sea menor que 10(cont< 10))
         
         
         System.out.println("+++++++++++++while++++++++++++++++");
-        int contador=0;
-        while (contador <10) {
-            System.out.println("hello World: "+contador);
-            contador++;
-        }
+        int contador=0;//inicio variable(int contador=0;)
+        while (contador <10) {//inicio ciclo(while) condicion(contador menor a 10(contador<10)) inicio ciclo({)
+            System.out.println("hello World: "+contador);//muestra por pantalla
+            contador++;//aumento del contador (contador++)
+        }//cierro ciclo(})
         
     }
     
