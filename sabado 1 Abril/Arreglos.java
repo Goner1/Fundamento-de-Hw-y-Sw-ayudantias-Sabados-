f46c5c1f588ead0a37b9c1633267700c6e8aa0df
Eduardo Queupumil F <e.queupumil01@ufromail.cl>
2017-04-01 12:22:52 -0300

    Update Arreglos.java

--- sabado 1 Abril/Arreglos.java
+++ sabado 1 Abril/Arreglos.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejemploclasesabado;
+package ejemploclaseSabadoSabado1Abril;
 
 import javax.swing.JOptionPane;
 
@@ -13,14 +13,13 @@ import javax.swing.JOptionPane;
  */
 public class Arreglos {
     public static void main(String[] args) {
-        int tempDias[]=new int[10];
-        int num;
-        for (int i = 0; i < tempDias.length; i++) {
-            tempDias[i]=Integer.parseInt(JOptionPane.showInputDialog(null,"ingrese numero espacio:"+i));
+        int tempDias[]=new int[5];//tipo(int) nombre(TempDias) []= instancia(new) tipo(int) cantidad([5]);
+        for (int i = 0; i < tempDias.length; i++) {//ciclo
+            tempDias[i]=Integer.parseInt(JOptionPane.showInputDialog(null,"ingrese numero espacio:"+i));//lectura de datos
             
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("dia"+i+" la temperatura fue de:"+tempDias[i]);     
+        for (int i = 0; i < tempDias.length; i++) {//ciclo
+            System.out.println("dia"+i+" la temperatura fue de:"+tempDias[i]); //muestra por consola(System.out.println) ("String" + variable(i)+ "String" + espacio del arreglo(tempDias[i]))   
         }
     }
 }
